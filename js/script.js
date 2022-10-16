8b3ea378ec5d2c7a831aa2a5b0b56ac98ed21131
bistm392 <115937727+bistm392@users.noreply.github.com>
2022-10-16 20:49:22 +0530

    sdfds
    sdfd

---
+++ js/script.js
@@ -0,0 +1,78 @@
+$(function() {
+  // Owl Carousel
+  var owl = $(".home-demo .owl-carousel");
+  owl.owlCarousel({
+    items: 3,
+    margin: 31.75,
+    loop: true,
+    dots:false,
+    navText: [
+        '<i class="fa-solid fa-arrow-left-long"></i>',
+        '<i class="fa-solid fa-arrow-right-long"></i>'
+    ],
+    nav: true,
+    responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 2,
+      nav: false,
+    },
+
+    1024: {
+      items: 3
+    },
+
+    1366: {
+      items: 4
+    }
+  }
+  });
+});
+
+$(function() {
+  // Owl Carousel
+  var owl = $(".bundle-slider .owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    dots:false,
+    nav: false,
+  });
+});
+
+$(function() {
+  // Owl Carousel
+  var owl = $(".testimonils-demo .owl-carousel");
+  owl.owlCarousel({
+    items: 3,
+    margin: 31.75,
+    loop: true,
+    dots:false,
+    navText: [
+        '<i class="fa-solid fa-arrow-left-long"></i>',
+        '<i class="fa-solid fa-arrow-right-long"></i>'
+    ],
+    nav: true,
+    responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 2,
+      nav: false,
+    },
+
+    1024: {
+      items: 3
+    },
+
+    1366: {
+      items: 3
+    }
+  }
+  });
+});
