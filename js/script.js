835fdc6f497ee832ecfdd119b3ecf7d1675800d3
bistm392 <115937727+bistm392@users.noreply.github.com>
2022-10-18 23:41:20 +0530

    fgf
    fgfg

--- js/script.js
+++ js/script.js
@@ -73,3 +73,35 @@ $(function() {
   }
   });
 });
+$(function() {
+  // Owl Carousel
+  var owl = $(".testimonils-product-demo .owl-carousel");
+  owl.owlCarousel({
+    items: 3,
+    margin: 31.75,
+    loop: true,
+    dots:false,
+    navText: [
+        '<i class="fa-solid fa-arrow-left-long"></i>',
+        '<i class="fa-solid fa-arrow-right-long"></i>'
+    ],
+    nav: false,
+    responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 2,
+    },
+
+    1024: {
+      items: 3
+    },
+
+    1366: {
+      items: 3
+    }
+  }
+  });
+});
